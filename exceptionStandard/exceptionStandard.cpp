3726120e333b57cf5ce839b91f4d256f4e1e3038
Jodinurf <jodinurfarkhani04@gmail.com>
2023-06-21 11:42:56 +0700

    error fix line 11

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 	cout << "awal program" << endl;			//penanda 1...
 	try {
-		array <int, 3> data = { 1,2,3 };
+		array <int, 3> data = { 3,5,7 };
 		//pesan array integer 3 elemen
 		cout<<data.at(5)<<endl;
 		//memanggil array elemen ke 5
