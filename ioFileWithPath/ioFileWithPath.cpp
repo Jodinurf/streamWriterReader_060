73f21997890b2ffd242fe9423996c6108918ff4b
Jodinurf <jodinurfarkhani04@gmail.com>
2023-06-21 11:21:06 +0700

    jika terdapat file, maka akan melakukan perulangan baris dengan getline dan menampilkan isi dari baris, jika sudah lalu file tersebut ditutup

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -37,5 +37,16 @@ int main() {
 	infile.open(namaFile, ios::in);
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
-
+	//jika file ada maka
+		if (infile.is_open())
+		{
+			//melakukan perulangan setiap baris
+			while (getline(infile, baris))
+			{
+				//dan tampilkan di sini
+				cout << baris << '\n';
+			}
+			// tutup file tersebut setelah selesai
+			infile.close();
+		}
 }
